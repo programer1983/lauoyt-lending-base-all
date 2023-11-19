b663e4d81fad4e54b459dccd0be74c448eabb57a
programer1983 <programer972@gmail.com>
2023-11-19 16:15:25 +0200

    create travels-technologies  block with tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+"use-strict"
+
+const tabItem = document.querySelectorAll('.travels-technologies__button')
+const tabContent = document.querySelectorAll('.travels-technologies__items-col')
+
+tabItem.forEach(function(element){
+  element.addEventListener('click', open)
+})
+
+function open(e){
+  const tabTarget = e.currentTarget
+  const button = tabTarget.dataset.button
+
+  tabItem.forEach(function(item){
+    item.classList.remove('travels-technologies__button--active"')
+  })
+
+  tabContent.forEach(function(item){
+    item.classList.remove('travels-technologies--active')
+  })
+
+  tabTarget.classList.add('travels-technologies__button--active')
+  document.querySelector(`#${button}`).classList.add('travels-technologies--active')
+}
+
+
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
@@ -8,3 +34,5 @@ const swiper = new Swiper('.swiper', {
       clickable: true,
     },
   });
+
+
